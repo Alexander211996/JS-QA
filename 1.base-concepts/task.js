a89d7a5bd4a699a83adf38d0fe31185b65c0bf75
Alexander <rubolov.alexander@mail.ru>
2022-11-28 20:48:13 +0300

    Base-concepts task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,16 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
-  return arr;
+  arr = [];
+  let d = (b**2-4*a*c);
+  if (d > 0){
+    arr.push((-b + Math.sqrt(d) )/(2*a));
+    arr.push((-b - Math.sqrt(d) )/(2*a));
+  } 
+  if (d === 0){
+    arr.push(-b/(2*a));  
+  }
+  return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
